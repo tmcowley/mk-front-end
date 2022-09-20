82402c8fd837e1f17137a35256a9be96988a7976
Thomas Cowley <thomas.cowley19@gmail.com>
2022-09-20 23:25:07 +0100

    network hotfix

--- src/constants/api-config.ts
+++ src/constants/api-config.ts
@@ -31,8 +31,8 @@ const axiosGetConfigGenerator = (params?: {}): AxiosRequestConfig<string> => {
 export const apiConfig: apiConfigType = {
   apiVersion: "/api/v0/",
   // host: "error",
-  host: "http://localhost:8080",
-  // host: "https://mirrored-keyboard.herokuapp.com",
+  // host: "http://localhost:8080",
+  host: "https://mirrored-keyboard.herokuapp.com",
   axiosGetConfigGenerator: axiosGetConfigGenerator,
   axiosPostConfig: axiosPostConfig,
   axiosGetConfig: axiosGetConfigGenerator(),
